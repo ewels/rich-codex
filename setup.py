a349b0effd6f2bb5ad463ea39835df78710f8219
Phil Ewels <phil@seqera.io>
2022-05-22 23:27:21 +0200

    Move requirements into setup.py, make cairo an extra

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
-requirements = open("requirements.txt").read().splitlines()
-
 setup(
     name="rich-codex",
-    install_requires=requirements,
+    install_requires=[
+        "rich>=12.4.1",
+        "rich-cli>=1.8.0",
+        "rich-click>=1.4",
+        "importlib-metadata; python_version < '3.8'",
+    ],
+    extras_require={
+        "cairo": "CairoSVG>=2.5.2",
+        "dev": "pre-commit",
+    },
 )
