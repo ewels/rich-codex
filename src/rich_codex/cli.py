a8b5ae71bee10ee75629400699fb6582a82f7854
Phil Ewels <phil@seqera.io>
2022-05-23 23:42:02 +0200

    CLI help texts, non-verbose + colourful logs

--- src/rich_codex/cli.py
+++ src/rich_codex/cli.py
@@ -1,29 +1,97 @@
 import logging
+from os import getenv
 
+from rich.console import Console
 from rich.logging import RichHandler
 
 from rich_codex import codex_search, rich_img
 
 import rich_click as click
 
-logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(markup=True)])
+click.rich_click.OPTION_ENVVAR_FIRST = True
+click.rich_click.ENVVAR_STRING = "[env: {}]"
 
-log = logging.getLogger("rich-codex")
+log = logging.getLogger()
 
 
 @click.command()
 @click.argument("search_paths", nargs=-1)
-@click.option("--search-include", envvar="SEARCH_INCLUDE", show_envvar=True)
-@click.option("--search-exclude", envvar="SEARCH_EXCLUDE", show_envvar=True)
-@click.option("--no-search", is_flag=True, envvar="NO_SEARCH", show_envvar=True)
-@click.option("--command", envvar="COMMAND", show_envvar=True)
-@click.option("--snippet", envvar="SNIPPET", show_envvar=True)
-@click.option("--snippet-syntax", envvar="SNIPPET_SYNTAX", show_envvar=True)
-@click.option("--img-paths", envvar="IMG_PATHS", show_envvar=True)
-@click.option("--configs", envvar="RC_CONFIGS", show_envvar=True)
-@click.option("--no-confirm", is_flag=True, envvar="NO_CONFIRM", show_envvar=True)
-@click.option("--terminal-width", envvar="TERMINAL_WIDTH", show_envvar=True)
-@click.option("--terminal-theme", envvar="TERMINAL_THEME", show_envvar=True)
+@click.option(
+    "--search-include",
+    envvar="SEARCH_INCLUDE",
+    show_envvar=True,
+    help="Glob patterns to search for rich-codex comments",
+)
+@click.option(
+    "--search-exclude",
+    envvar="SEARCH_EXCLUDE",
+    show_envvar=True,
+    help="Glob patterns to exclude from search for rich-codex comments",
+)
+@click.option(
+    "--no-search",
+    is_flag=True,
+    envvar="NO_SEARCH",
+    show_envvar=True,
+    help="Set to disable searching for rich-codex comments",
+)
+@click.option(
+    "--command",
+    envvar="COMMAND",
+    show_envvar=True,
+    help="Specify a command to run to capture output",
+)
+@click.option(
+    "--snippet",
+    envvar="SNIPPET",
+    show_envvar=True,
+    help="Literal code snippet to render",
+)
+@click.option(
+    "--snippet-syntax",
+    envvar="SNIPPET_SYNTAX",
+    show_envvar=True,
+    help="Language to use for snippet sytax highlighting",
+)
+@click.option(
+    "--img-paths",
+    envvar="IMG_PATHS",
+    show_envvar=True,
+    help="Path to image filenames if using 'command' or 'snippet'",
+)
+@click.option(
+    "--configs",
+    envvar="RC_CONFIGS",
+    show_envvar=True,
+    help="Paths to YAML config files",
+)
+@click.option(
+    "--no-confirm",
+    is_flag=True,
+    envvar="NO_CONFIRM",
+    show_envvar=True,
+    help="Set to skip confirmation prompt before running commands",
+)
+@click.option(
+    "--terminal-width",
+    envvar="TERMINAL_WIDTH",
+    show_envvar=True,
+    help="Width of the terminal",
+)
+@click.option(
+    "--terminal-theme",
+    envvar="TERMINAL_THEME",
+    show_envvar=True,
+    help="Colour theme",
+)
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    envvar="LOG_VERBOSE",
+    show_envvar=True,
+    help="Print verbose output to the console.",
+)
 def main(
     search_paths,
     search_include,
@@ -37,8 +105,27 @@ def main(
     no_confirm,
     terminal_width,
     terminal_theme,
+    verbose,
 ):
     """Run the click command-line interface."""
+    # Set up the logger
+    log.setLevel(logging.DEBUG if verbose else logging.INFO)
+
+    # Set up logs to the console
+    log.addHandler(
+        RichHandler(
+            console=Console(
+                stderr=True,
+                force_terminal=(
+                    True if getenv("GITHUB_ACTIONS") or getenv("FORCE_COLOR") or getenv("PY_COLORS") else None
+                ),
+                terminal_width=getenv("TERMINAL_WIDTH"),
+            ),
+            show_time=False,
+            markup=True,
+        )
+    )
+
     # Check for mutually exclusive options
     if command and snippet:
         raise click.BadOptionUsage("--command", "Please use either --command OR --snippet but not both")
