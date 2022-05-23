838db1710a3c12540c07251b7504e4ad1d282387
Phil Ewels <phil@seqera.io>
2022-05-23 23:45:49 +0200

    Fix console argument name

--- src/rich_codex/cli.py
+++ src/rich_codex/cli.py
@@ -119,7 +119,7 @@ def main(
                 force_terminal=(
                     True if getenv("GITHUB_ACTIONS") or getenv("FORCE_COLOR") or getenv("PY_COLORS") else None
                 ),
-                terminal_width=getenv("TERMINAL_WIDTH"),
+                width=getenv("TERMINAL_WIDTH"),
             ),
             show_time=False,
             markup=True,
