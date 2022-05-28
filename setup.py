6627030ea86efbebf5014db77f127417ecc5aacb
Phil Ewels <phil@seqera.io>
2022-05-28 21:30:28 +0200

    Don't use pre-release rich-click

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="rich-codex",
     install_requires=[
         "rich>=12.4.3",
-        # "rich-click>=1.5",
-        "rich-click @ git+https://github.com/ewels/rich-click@main#egg=rich-click",
+        "rich-click>=1.4",
         "importlib-metadata; python_version < '3.8'",
     ],
     extras_require={
