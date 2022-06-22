db71d5a918383e8e624f3782458c5e249edde373
Phil Ewels <phil@seqera.io>
2022-06-23 00:43:21 +0200

    Cache deps in action, use stable rich-click release

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
     name="rich-codex",
     install_requires=[
         "rich>=12.4.3",
-        # "rich-click>=1.5",
-        "rich-click @ git+https://github.com/ewels/rich-click@main#egg=rich-click",
+        "rich-click>=1.5",
         "rich-cli",  # For convience only
         "importlib-metadata; python_version < '3.8'",
         "levenshtein>=0.18.1",
