d48e6df4543ec6b5da13b29ceb1a6ccc46d2707e
Phil Ewels <phil@seqera.io>
2022-07-07 00:40:12 +0200

    Add pyyaml as a requirement

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         "rich-click>=1.5",
         "rich-cli",  # For convience only
         "importlib-metadata; python_version < '3.8'",
+        "pyyaml",
         "levenshtein>=0.18.1",
         "jsonschema",
         "GitPython",
