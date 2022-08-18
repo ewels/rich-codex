11e76e7e3871ec8b9f6000480853d4f4eaa17dbd
Phil Ewels <phil@seqera.io>
2022-08-18 15:27:14 +0200

    Bump to v1.2.3

--- src/rich_codex/__init__.py
+++ src/rich_codex/__init__.py
@@ -5,6 +5,6 @@ It should work as a standalone command-line tool, however it is primarily intend
 for use with the rich-codex GitHub Action.
 """
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 
 from rich_codex import rich_img  # noqa: F401
